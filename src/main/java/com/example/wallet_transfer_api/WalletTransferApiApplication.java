1bae27e7e1cea44bd5c746bb6bfc8f8c7447f24d
Joel Neto <joel.antoniete@gmail.com>
2026-02-12 20:38:49 -0300

    configuração inicial

---
+++ src/main/java/com/example/wallet_transfer_api/WalletTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallet_transfer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletTransferApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wallet_transfer_api/WalletTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallet_transfer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
