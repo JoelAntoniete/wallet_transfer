1744df7bb5a7afc38e64e0660b930539bfe8d99c
Joel Neto <joel.antoniete@gmail.com>
2026-02-14 00:10:14 -0300

    finalizando exception

---
+++ src/main/java/com/example/wallet_transfer_api/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.example.wallet_transfer_api.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
---
+++ src/main/java/com/example/wallet_transfer_api/infra/ControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.wallet_transfer_api.infra;
+
+import com.example.wallet_transfer_api.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuario cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatEsception(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exception);
+    }
+}
